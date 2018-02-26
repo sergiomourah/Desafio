38cdd3e8aab8e2d376b51c7e60e42f9aa1128bc2
Unknown <sergio.moura.h@hotmail.com>
2018-02-26 17:33:08 -0300

    Inicio do desenvolvimento front-end

--- DesafioDev/src/main/ts/src/app/app.module.ts
+++ DesafioDev/src/main/ts/src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -8,18 +9,49 @@ import { AppComponent } from './app.component';
 import { GeneratedModule } from '../generated/generated.module';
 import { BROKER_CONFIGURATION } from '../generated/services-wrapper';
 import { ContratoComponent } from './contrato/contrato.component';
+import { InicialComponent } from './inicial/inicial.component';
+
+import { CovalentLayoutModule } from '@covalent/core/layout';
+import { CovalentStepsModule  } from '@covalent/core/steps';
+/* any other core modules */
+// (optional) Additional Covalent Modules imports
+import { CovalentHttpModule } from '@covalent/http';
+import { CovalentHighlightModule } from '@covalent/highlight';
+import { CovalentMarkdownModule } from '@covalent/markdown';
+import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
+import { MatFormFieldModule, MatInputModule, MatCheckboxModule, MatButtonModule } from '@angular/material';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { CovalentExpansionPanelModule } from '@covalent/core/expansion-panel';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ContratoComponent,
+    InicialComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule,    FormsModule,
+
     GeneratedModule,
     HttpModule,
+    FormsModule,    
+    MatFormFieldModule,
+    MatInputModule,    FormsModule,
+
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    CovalentLayoutModule,
+    CovalentStepsModule,
+    // (optional) Additional Covalent Modules imports
+    CovalentHttpModule.forRoot(),
+    CovalentHighlightModule,
+    CovalentMarkdownModule,
+    CovalentDynamicFormsModule,
+    CovalentExpansionPanelModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [{
     provide: BROKER_CONFIGURATION,
